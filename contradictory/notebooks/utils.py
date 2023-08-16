trainable model, maybe a gradient bug in accelerator?

contradictory/notebooks/utils.py
@@ -1,8 +1,9 @@
-
+import os
 
 # Constants
 DATA_PATH = "data"
 WANDB_PROJECT = "contradictory"
+ENTITY = None
 RAW_DATA_AT = "contra_raw"
 PROCESSED_DATA_AT = "contra_split"
 
@@ -10,11 +11,3 @@ id2label = {0: "entailment", 1: "neutral", 2: "contradiction"}
 label2id = {v:k for k,v in id2label.items()}
 
 SEED = 98765
-
-def seed_everything(seed):
-    os.environ['PYTHONHASHSEED']=str(seed)
-    torch.manual_seed(seed)
-    np.random.seed(seed)
-    # random.seed(seed)
-    
-seed_everything(SEED)